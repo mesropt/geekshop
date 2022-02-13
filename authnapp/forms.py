0d31d836a2cc4216ed57230cb3d69f4c2b80ce7f
Mesrop <mesroptnamak@gmail.com>
2022-02-13 18:45:39 +0300

    lesson_10. User's profile edit

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -4,7 +4,7 @@ import random
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 
-from .models import ShopUser, ShopUserProfile
+from .models import ShopUser
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -63,14 +63,3 @@ class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
         fields = ("username", "first_name", "email", "age", "avatar")
-
-
-class ShopUserProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = ShopUserProfile
-        fields = ("tagline", "aboutMe", "gender")
-
-    def __init__(self, *args, **kwargs):
-        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
