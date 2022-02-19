4e2b23ff5486eba9e838b2d484a890fd8b29df2c
Mesrop <74879366+mesropt@users.noreply.github.com>
2022-02-19 16:53:56 +0300

    Lesson 11 (#4)
    Lesson 11 ready.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
 
 # Auth model
@@ -76,9 +77,10 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
-            ],
+                "django.template.context_processors.media",
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = "geekshop.wsgi.application"
@@ -101,16 +103,16 @@ DATABASES = {
 if not DEBUG:
     AUTH_PASSWORD_VALIDATORS = [
         {
-            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
         },
         {
-            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"
         },
         {
-            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
         },
         {
-            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
         },
     ]
 else:
