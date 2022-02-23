da11a348b0c79656b06edf676488a51675a037e1
Mesrop <74879366+mesropt@users.noreply.github.com>
2022-02-23 10:35:32 +0300

    Lesson 13 (#6)
    Lesson_13. Ready.

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,6 +1,4 @@
 from django.db import transaction
-from django.db.models.signals import pre_delete, pre_save
-from django.dispatch import receiver
 from django.forms import inlineformset_factory
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -38,7 +36,6 @@ class OrderItemsCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial["product"] = basket_items[num].product
                     form.initial["quantity"] = basket_items[num].quantity
-                    form.initial["price"] = basket_items[num].product.price
             else:
                 formset = OrderFormSet()
 
@@ -85,11 +82,7 @@ class OrderItemsUpdate(UpdateView):
         if self.request.POST:
             data["orderitems"] = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(instance=self.object)
-            for form in formset.forms:
-                if form.instance.pk:
-                    form.initial["price"] = form.instance.product.price
-            data["orderitems"] = formset
+            data["orderitems"] = OrderFormSet(instance=self.object)
         return data
 
     def form_valid(self, form):
@@ -122,20 +115,14 @@ def order_forming_complete(request, pk):
     return HttpResponseRedirect(reverse("ordersapp:orders_list"))
 
 
-@receiver(pre_save, sender=OrderItem)
-@receiver(pre_save, sender=Basket)
-def product_quantity_update_save(instance, sender, **kwargs):
-    if instance.pk:
-        """If user change quantity in order or basket"""
-        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
-    else:
-        """If user create order or basket"""
-        instance.product.quantity -= instance.quantity
-    instance.product.save()
+from mainapp.models import Product
+from django.http import JsonResponse
 
 
-@receiver(pre_delete, sender=OrderItem)
-@receiver(pre_delete, sender=Basket)
-def product_quantity_update_delete(instance, **kwargs):
-    instance.product.quantity += instance.quantity
-    instance.product.save()
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({"price": product.price})
+        else:
+            return JsonResponse({"price": 0})
