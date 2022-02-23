89b90f2cb959c80e7c0bd957e8e5dbd9737db9e7
Mesrop <mesroptnamak@gmail.com>
2022-02-23 12:02:18 +0300

    lesson_14. Testing tools

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -195,13 +195,17 @@ SOCIAL_AUTH_GITHUB_SECRET = github_auth["client_secret"]
 
 # Django Debug Toolbar --->
 if DEBUG:
-    INSTALLED_APPS.extend(["debug_toolbar", "template_profiler_panel"])
+    INSTALLED_APPS.extend([
+        "debug_toolbar",
+        "template_profiler_panel",
+        "django_extensions",
+    ])
 
 
 if DEBUG:
     MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])
 
-# Debgu tool bar settings
+# Debug tool bar settings
 if DEBUG:
 
     def show_toolbar(request):
